129ba5bd5818b102ad56636ff69af2d51b75250f
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-08 19:51:35 -0800

    Add get, post methods

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: "Hello from the server side!",
-        app: "API"
-    })
+// Middleware for POST methods to work
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-    res.send("You can post to this endpoint")
-})
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+
+  // Merges two objects together
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  // Write into a file
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).jsonp({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 4000;
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
+  console.log(`Server listening on port ${port}`);
+});
