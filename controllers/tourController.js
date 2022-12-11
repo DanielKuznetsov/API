8743a2ced0645c393e012df8f1ba23f6201cb973
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-10 17:07:23 -0800

    Advanced aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -150,3 +150,57 @@ exports.getTourStats = async (req, res, next) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates", // this will create -1 document for each of the dates
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numToursStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
+      {
+        $addFields: { month: "$_id" },
+      },
+      {
+        $project: {
+          _id: 0, // 1 will make the id to show up
+        },
+      },
+      {
+        $sort: { numToursStarts: 1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      results: plan.length,
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
