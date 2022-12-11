db6052af5b5f24ac1789e17b975e3750d3a9b958
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-10 16:42:56 -0800

    Create a stats' aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,5 @@
 const Tour = require("./../models/tourModel.js");
-const APIfeatures = require('./../utils/apiFeatures.js')
+const APIfeatures = require("./../utils/apiFeatures.js");
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
@@ -103,6 +103,45 @@ exports.deleteTour = async (req, res) => {
     res.status(204).json({
       status: "success",
       data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res, next) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: "$ratingsQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: "EASY" } }, // excluding EASY documents
+      // },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        stats,
+      },
     });
   } catch (err) {
     res.status(404).json({
