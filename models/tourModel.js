06db3894c149c9a05e0ae31e3a5234fce99743b7
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-11 17:43:11 -0800

    Add aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -80,11 +80,19 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function(docs, next) {
+tourSchema.post(/^find/, function (docs, next) {
   // console.log(`Query took: ${Date.now() - this.start} milliseconds`);
 
   next();
-})
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre("aggregate", function (next) {
+  // removing all documents from the output that have secretTour set to true
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  next();
+});
 
 const Tour = mongoose.model("Tour", tourSchema);
 
