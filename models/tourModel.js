0b9ac7892c50b3ecc938abb26c557ed27dc27736
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-11 18:03:37 -0800

    Add built-in and custom validators to the tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name!"],
       unique: true,
       trim: true,
+      maxlength: [40, "A tour name must have less or equal than 40 characters"],
+      minlength: [10, "A tour name must have more than 10 characters"],
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty!"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either easy, medium, or difficult",
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
     },
     ratingsQuantity: {
       type: Number,
@@ -34,7 +42,18 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price!"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // -this will point to the current document on NEW document creation
+          // -val is the value that the user inputted
+          return val <= this.price; // -> this is not going to work on UPDATE
+          // 100 < 200 -> true, no error or validation error will be triggered
+        },
+        message: "Discount price ({VALUE}) should be below the regular price",
+      },
+    },
     summary: {
       type: String,
       trim: true,
